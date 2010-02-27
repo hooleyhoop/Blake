c1dbc89d9acc19fa861adcff1342fde4fdebf9d8
HooleyHoop <steven.hooley@gmail.com>
2010-02-27 21:27:50 +0000

    runtest.sh

--- runTests.sh
+++ runTests.sh
@@ -6,5 +6,8 @@ export NSHangOnUncaughtException=YES
 export NSAutoreleaseFreedObjectCheckEnabled=YES
 export MallocScribble=1
 export MallocPreScribble=1
-
+export OBJC_DISABLE_GC=YES
+export DYLD_PRINT_LIBRARIES=1
+export DYLD_PRINT_OPTS=1
+export DYLD_PRINT_INITIALIZERS=1
 "${SYSTEM_DEVELOPER_DIR}/Tools/RunUnitTests"
